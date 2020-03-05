5508a09763fe70a82bc9250b6a54c652e71ab3d0
shizhongyu <shizhongyu@yibaogao.com>
2020-03-05 17:48:32 +0800

    docker-test

--- src/main/java/com/shizy/ShizyApplication.java
+++ src/main/java/com/shizy/ShizyApplication.java
@@ -9,6 +9,11 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})//不用数据库 排除啦
 public class ShizyApplication {
     public static void main(String[] args) {
+
+        System.out.println("========================");
+        System.out.println("docker作业：石中玉");
+        System.out.println("========================");
+
         SpringApplication.run(ShizyApplication.class, args);
     }
 }
