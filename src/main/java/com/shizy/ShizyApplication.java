920c6b973bb0ef95e659287940a3eaec913d7a01
zhongwang <zhongwang@qq.com>
2020-07-13 00:29:51 +0800

    清除不必要项

--- src/main/java/com/shizy/ShizyApplication.java
+++ src/main/java/com/shizy/ShizyApplication.java
@@ -10,11 +10,6 @@ import org.springframework.cache.annotation.EnableCaching;
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})//不用数据库 排除啦
 public class ShizyApplication {
     public static void main(String[] args) {
-
-//        System.out.println("========================");
-//        System.out.println("docker作业：石中玉");
-//        System.out.println("========================");
-
         SpringApplication.run(ShizyApplication.class, args);
     }
 }
